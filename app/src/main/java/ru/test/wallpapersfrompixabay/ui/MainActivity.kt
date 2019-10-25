b73cb56509a9a503fda9b3f4f17707be76eb4735
Ruslan <wazsone@ya.ru>
2019-10-25 22:35:54 +0700

    feature: set image as wallpaper

--- app/src/main/java/ru/test/wallpapersfrompixabay/ui/MainActivity.kt
+++ app/src/main/java/ru/test/wallpapersfrompixabay/ui/MainActivity.kt
@@ -21,6 +21,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
         initViews()
         initViewModel()
     }
--- app/src/main/java/ru/test/wallpapersfrompixabay/ui/adapters/ImagesRowAdapter.kt
+++ app/src/main/java/ru/test/wallpapersfrompixabay/ui/adapters/ImagesRowAdapter.kt
@@ -1,17 +1,27 @@
 package ru.test.wallpapersfrompixabay.ui.adapters
 
+import android.app.WallpaperManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.images_row.view.*
-import ru.test.wallpapersfrompixabay.R
 import ru.test.wallpapersfrompixabay.models.Image
+import java.io.IOException
+
 
 class ImagesRowAdapter(private var images: List<Image>) :
     RecyclerView.Adapter<ImagesRowAdapter.ImagesRowViewHolder>() {
+
+    private val TAG_ID = ImagesRowAdapter::class.java.simpleName
 
     fun setImages(images: List<Image>) {
         this.images = images
@@ -20,7 +30,7 @@ class ImagesRowAdapter(private var images: List<Image>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ImagesRowViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.images_row,
+                ru.test.wallpapersfrompixabay.R.layout.images_row,
                 parent,
                 false
             )
@@ -32,10 +42,69 @@ class ImagesRowAdapter(private var images: List<Image>) :
         holder.bind(images[position])
 
     inner class ImagesRowViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         fun bind(image: Image) {
             Picasso.get().load(image.previewURL)
-                .placeholder(R.drawable.placeholder)
+                .placeholder(ru.test.wallpapersfrompixabay.R.drawable.placeholder)
                 .into(itemView.image_item_iv as ImageView)
+
+            itemView.setOnClickListener {
+                val builder =
+                    AlertDialog.Builder(itemView.context)
+                        .setTitle(ru.test.wallpapersfrompixabay.R.string.alert_title)
+                        .setMessage(ru.test.wallpapersfrompixabay.R.string.alert_ok_dialog_msg)
+                        .setNeutralButton(ru.test.wallpapersfrompixabay.R.string.alert_btn_cancel_name) { _, _ -> }
+                        .setPositiveButton(ru.test.wallpapersfrompixabay.R.string.alert_btn_ok_name) { _, _ ->
+                            getAndSetWallPaper(image.largeImageURL!!)
+                        }
+
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            }
+        }
+
+        private fun getAndSetWallPaper(imageUrl: String) {
+            Picasso.get().load(imageUrl)
+                .into(object : Target {
+                    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                        Toast.makeText(
+                            itemView.context,
+                            itemView.context.getString(ru.test.wallpapersfrompixabay.R.string.alert_preparing_wallpaper_set),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onBitmapFailed(
+                        e: Exception?,
+                        errorDrawable: Drawable?
+                    ) {
+                        Toast.makeText(
+                            itemView.context,
+                            itemView.context.getString(ru.test.wallpapersfrompixabay.R.string.alert_failed_wallpaper_set),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onBitmapLoaded(
+                        bitmap: Bitmap?,
+                        from: Picasso.LoadedFrom?
+                    ) {
+                        val message = try {
+                            WallpaperManager.getInstance(itemView.context)
+                                .setBitmap(bitmap)
+                            itemView.context.getString(ru.test.wallpapersfrompixabay.R.string.alert_success_wallpaper_set)
+                        } catch (e: IOException) {
+                            Log.d(TAG_ID, "FAILED to set wallpaper! ERROR: ${e}")
+                            itemView.context.getString(ru.test.wallpapersfrompixabay.R.string.alert_failed_wallpaper_set)
+                        }
+                        Toast.makeText(
+                            itemView.context,
+                            message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                })
         }
     }
 
